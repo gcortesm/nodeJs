e037362c811fdd724b7b641ca6fdca59931545df
Gustavo Adolfo Cortes Mondragon <gucortez@personalsoft.com.co>
2021-03-29 16:05:57 -0500

    Agregando babel y Nx que me faltaron los ejercicios practicos

---
+++ NODEJS/AutoAprendizaje/Babel/lib/Prueba01.js
@@ -0,0 +1,43 @@
+"use strict";
+
+require("core-js/modules/es.array.map.js");
+
+require("core-js/modules/es.promise.finally.js");
+
+require("core-js/modules/es.promise.js");
+
+require("core-js/modules/es.object.to-string.js");
+
+var _http = require("http");
+
+var hostname = '127.0.0.1'; //Linux
+//export PORT=3000
+//Windows
+//SET PORT=3000
+
+
+var port = process.env.PORT;
+[1, 2, 3].map(function (n) {
+  return n + 1;
+});
+
+var server = (0, _http.createServer)(function (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+}); //Crea un servidor con NodeJS
+
+server.listen(port, hostname, function () {
+  console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
+}); //Ejemplo de polyfills
+
+Promise.resolve().finally(); //Otro ejemplo de polyfills
+
+var timingpromise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve("Promise is resolved!");
+  }, 1000);
+});
+timingpromise.then(function (msg) {
+  console.log("%c" + msg, "font-size:25px;color:red;");
+});
---
+++ NODEJS/AutoAprendizaje/Babel/src/Prueba01.js
@@ -0,0 +1,34 @@
+import { createServer } from 'http'
+
+const hostname = '127.0.0.1'
+//Linux
+//export PORT=3000
+//Windows
+//SET PORT=3000
+const port = process.env.PORT;
+
+[1, 2, 3].map((n) => n + 1);
+
+const server = createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World!\n')
+})  //Crea un servidor con NodeJS
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
+//Ejemplo de polyfills
+Promise.resolve().finally();
+
+//Otro ejemplo de polyfills
+let timingpromise = new Promise((resolve, reject) => {
+  setTimeout(function() {
+     resolve("Promise is resolved!");
+  }, 1000);
+});
+
+timingpromise.then((msg) => {
+  console.log("%c"+msg, "font-size:25px;color:red;");
+});
